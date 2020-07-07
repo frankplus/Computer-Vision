dc18946ad176d9abe9f7995384568060f0cab9cd
Francesco <pham.francesco@gmail.com>
2020-07-07 15:37:37 +0200

    add trackbars

--- include/finalproject.h
+++ include/finalproject.h
@@ -1,6 +1,26 @@
 #ifndef __FINALPROJECT__
 #define __FINALPROJECT__
 
+#include <opencv2/core.hpp>
+#include <opencv2/objdetect.hpp>
+
+using namespace cv;
+
+struct DetectionParams
+{
+    CascadeClassifier tree_cascade;
+    Mat input_image;
+    Mat filtered_image;
+    Range hue_range{96, 128};
+    int filter_sigma = 100;
+    int min_neighbors = 5;
+    int min_size = 50;
+    int group_thresh = 0;
+    int group_eps = 20;
+};
+
 void main_finalproject();
+void preprocess_image(Mat input, Mat &result, double sigma, Range hue_range);
+void detect_and_display( DetectionParams *params );
 
 #endif
--- src/finalproject.cpp
+++ src/finalproject.cpp
@@ -9,90 +9,92 @@
 using namespace std;
 using namespace cv;
 
-const string path = "data/final_project/*.jpg";
-const String winname = "Detection";
-CascadeClassifier tree_cascade;
+const String cascade_path = "data/final_project/cascade.xml";
+const string images_path = "data/final_project/*.jpg";
+const String detection_winname = "Detection";
+const String result_winname = "Result";
 
-struct FilterParams
-{
-    Mat image;
-    Range hue_range;
-};
-
-void detect_and_display( Mat frame_gray, Mat frame_orig )
-{
-    equalizeHist( frame_gray, frame_gray );
-
-    std::vector<Rect> trees;
-    Mat result = frame_orig.clone();
-
-    tree_cascade.detectMultiScale( frame_gray, trees, 1.05, 10, 0, Size(100,100));
-    // groupRectangles(trees, 1, 0.85);
-    for ( size_t i = 0; i < trees.size(); i++ )
-    {
-        Point pt1(trees[i].x, trees[i].y);
-        Point pt2(trees[i].x + trees[i].width, trees[i].y + trees[i].height);
-        rectangle(frame_gray, pt1, pt2, Scalar(128,0,0), 2);
-        rectangle(result, pt1, pt2, Scalar(128,0,0), 2);
-    }
-
-    imshow( winname, frame_gray );
-    imshow( "result", result );
-}
 
 static void on_trackbar_change(int, void *params)
 {
-    FilterParams *filter_params = static_cast<FilterParams*>(params);
+    DetectionParams *filter_params = static_cast<DetectionParams*>(params);
 
-    Mat image_HSV, mask, masked_image, image_gray;
+    preprocess_image(filter_params->input_image, filter_params->filtered_image, 
+                    filter_params->filter_sigma, filter_params->hue_range);
 
-    Mat image = filter_params->image;
-    // bilateralFilter(filter_params->image, image, 3, 50,50);
-
-    cvtColor(image, image_HSV, COLOR_BGR2HSV);
-    Scalar lower_bound(filter_params->hue_range.start-1, 0, 0);
-    Scalar upper_bound(filter_params->hue_range.end, 255, 255);
-    inRange(image_HSV, lower_bound, upper_bound, mask);
-
-    cvtColor( image, image_gray, COLOR_BGR2GRAY );
-    bitwise_not(mask, mask);
-    bitwise_or(image_gray, mask, masked_image);
-
-    detect_and_display(masked_image, filter_params->image);
-
-    // cvtColor( filter_params->image, image_gray, COLOR_BGR2GRAY );
-    // detect_and_display(image_gray, filter_params->image);
+    detect_and_display(filter_params);
 }
+
 
 void main_finalproject() 
 {
+    namedWindow(detection_winname);
+    namedWindow(result_winname);
 
-    if( !tree_cascade.load( "data/final_project/cascade.xml" ) )
+    DetectionParams params;
+    if( !params.tree_cascade.load( cascade_path ) )
     {
         cout << "Error loading cascade\n";
         return;
     }
 
+    createTrackbar("Hue lower bound", detection_winname, &params.hue_range.start, 128, on_trackbar_change, (void*)&params);
+    createTrackbar("Hue upper bound", detection_winname, &params.hue_range.end, 128, on_trackbar_change, (void*)&params);
+    createTrackbar("Filter sigma range/space", detection_winname, &params.filter_sigma, 200, on_trackbar_change, (void*)&params);
+    createTrackbar("Detection min neighbors", detection_winname, &params.min_neighbors, 40, on_trackbar_change, (void*)&params);
+    createTrackbar("Detection min size", detection_winname, &params.min_size, 100, on_trackbar_change, (void*)&params);
+    createTrackbar("Group threshold", detection_winname, &params.group_thresh, 20, on_trackbar_change, (void*)&params);
+    createTrackbar("Group eps", detection_winname, &params.group_eps, 40, on_trackbar_change, (void*)&params);
+
     // load images
     vector<String> images_paths;
-    glob(path, images_paths);
-
-    namedWindow(winname);
-
-
-    FilterParams params;
-    params.hue_range.start = 0;
-    params.hue_range.end = 128;
-    createTrackbar("hue lower bound", winname, &params.hue_range.start, 128, on_trackbar_change, (void*)&params);
-    createTrackbar("hue upper bound", winname, &params.hue_range.end, 129, on_trackbar_change, (void*)&params);
+    glob(images_path, images_paths);
 
     for (String imgpath: images_paths) 
     {
         cout << imgpath << endl;
         Mat image = imread(imgpath);
-        resize(image, image, Size(400,500));
-        params.image = image;
+        resize(image, image, Size(500, 500));
+        params.input_image = image;
         on_trackbar_change(0, &params);
         waitKey(0);
     }
+}
+
+void detect_and_display( DetectionParams *params )
+{
+    Mat result = params->input_image.clone();
+
+    vector<Rect> trees;
+    Size min_size{params->min_size, params->min_size};
+    params->tree_cascade.detectMultiScale( params->filtered_image, trees, 1.05, params->min_neighbors, 0, min_size);
+    groupRectangles(trees, params->group_thresh, params->group_eps/100.0);
+
+    for ( size_t i = 0; i < trees.size(); i++ )
+    {
+        Point pt1(trees[i].x, trees[i].y);
+        Point pt2(trees[i].x + trees[i].width, trees[i].y + trees[i].height);
+        rectangle(params->filtered_image, pt1, pt2, Scalar(128,0,0), 2);
+        rectangle(result, pt1, pt2, Scalar(128,0,0), 2);
+    }
+
+    imshow( detection_winname, params->filtered_image );
+    imshow( result_winname, result );
+}
+
+void preprocess_image(Mat input, Mat &result, double sigma, Range hue_range)
+{
+    Mat filtered;
+    bilateralFilter(input, filtered, 11, sigma, sigma);
+
+    Mat image_HSV;
+    cvtColor(filtered, image_HSV, COLOR_BGR2HSV);
+
+    Mat mask;
+    Scalar lower_bound(hue_range.start, 0, 0);
+    Scalar upper_bound(hue_range.end, 255, 255);
+    inRange(image_HSV, lower_bound, upper_bound, mask);
+
+    cvtColor( filtered, result, COLOR_BGR2GRAY );
+    result.setTo(Scalar(128,128,128), mask);
 }
