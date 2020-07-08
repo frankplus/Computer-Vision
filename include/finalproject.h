7bda81e064fe2089c1f28c57ba2a092c33ce44df
Francesco <pham.francesco@gmail.com>
2020-07-08 19:34:53 +0200

    start report and add comments

--- include/finalproject.h
+++ include/finalproject.h
@@ -11,17 +11,16 @@ struct DetectionParams
     CascadeClassifier tree_cascade;
     Mat input_image;
     Mat filtered_image;
-    Range hue_range{36, 195};
-    Range value_range{89, 256};
+    Range hue_range{0, 195};
+    Range value_range{92, 256};
     int filter_sigma = 61;
-    int min_neighbors = 4;
+    int min_neighbors = 7;
     int min_size = 90;
-    int group_thresh = 0;
-    int group_eps = 30;
+    int delta_brightness = 45;
 };
 
 void main_finalproject();
-void preprocess_image(Mat input, Mat &result, double sigma, Range hue_range, Range value_range);
+void preprocess_image(Mat input, Mat &result, double sigma, Range hue_range, Range value_range, int delta_brightness);
 void detect_and_display( DetectionParams *params );
 void print_parameters(DetectionParams *filter_params);
 
--- src/finalproject.cpp
+++ src/finalproject.cpp
@@ -15,7 +15,6 @@ const String detection_winname = "Detection";
 const String result_winname = "Result";
 
 
-
 static void on_trackbar_change(int, void *params)
 {
     DetectionParams *filter_params = static_cast<DetectionParams*>(params);
@@ -23,7 +22,8 @@ static void on_trackbar_change(int, void *params)
     print_parameters(filter_params);
 
     preprocess_image(filter_params->input_image, filter_params->filtered_image, 
-                    filter_params->filter_sigma, filter_params->hue_range, filter_params->value_range);
+                    filter_params->filter_sigma, filter_params->hue_range, 
+                    filter_params->value_range, filter_params->delta_brightness);
 
     detect_and_display(filter_params);
 }
@@ -35,6 +35,8 @@ void main_finalproject()
     namedWindow(result_winname);
 
     DetectionParams params;
+
+    // load cascade classifier
     if( !params.tree_cascade.load( cascade_path ) )
     {
         cout << "Error loading cascade\n";
@@ -48,6 +50,7 @@ void main_finalproject()
     createTrackbar("Filter sigma range/space", detection_winname, &params.filter_sigma, 200, on_trackbar_change, (void*)&params);
     createTrackbar("Detection min neighbors", detection_winname, &params.min_neighbors, 40, on_trackbar_change, (void*)&params);
     createTrackbar("Detection min size", detection_winname, &params.min_size, 100, on_trackbar_change, (void*)&params);
+    createTrackbar("Delta brightness", detection_winname, &params.delta_brightness, 100, on_trackbar_change, (void*)&params);
 
     // load images
     vector<String> images_paths;
@@ -67,14 +70,22 @@ void main_finalproject()
     }
 }
 
+/**
+ * Detect the trees using cascade classifier at multi scale. Draw rectangles around the 
+ * detected trees and show the result both on the preprocessed image and on the original image.
+ * @param filter_params Pointer to DetectionParams struct containing original and preprocessed 
+ *                      image and detection parameters
+ */
 void detect_and_display( DetectionParams *params )
 {
     Mat result = params->input_image.clone();
 
+    // detect
     vector<Rect> trees;
     Size min_size{params->min_size, params->min_size};
     params->tree_cascade.detectMultiScale( params->filtered_image, trees, 1.05, params->min_neighbors, 0, min_size);
 
+    // draw bounding boxes
     for ( size_t i = 0; i < trees.size(); i++ )
     {
         Point pt1(trees[i].x, trees[i].y);
@@ -87,33 +98,53 @@ void detect_and_display( DetectionParams *params )
     imshow( result_winname, result );
 }
 
-void preprocess_image(Mat input, Mat &result, double sigma, Range hue_range, Range value_range)
+/**
+ * Preprocess image before the detection phase. The operations are histogram equalization, 
+ * bilateral filter and brighten up pixels in a given hue and value range.
+ * @param input The input image
+ * @param result The output image
+ * @param sigma Sigma color and sigma space for the bilateral filter
+ * @param hue_range The pixels inside this hue range are brighten up
+ * @param value_range The pixels inside this value range are brighten up
+ */
+void preprocess_image(Mat input, Mat &result, double sigma, Range hue_range, Range value_range, int delta_brightness)
 {
+    // split into RGB channels
     vector<Mat> img_channels;
     split(input, img_channels);
 
+    // equalize histograms
     equalizeHist(img_channels[0], img_channels[0]);
     equalizeHist(img_channels[1], img_channels[1]);
     equalizeHist(img_channels[2], img_channels[2]);
 
+    // merge back into single equalized RGB image
     Mat equalized_img;
     merge(img_channels, equalized_img);
 
+    // apply bilateral filter
     Mat filtered;
     bilateralFilter(equalized_img, filtered, 11, sigma, sigma);
 
+    // convert into HSV space
     Mat image_HSV;
     cvtColor(filtered, image_HSV, COLOR_BGR2HSV);
 
+    // select pixels of the image with hue and value in a given range
     Mat mask;
     Scalar lower_bound(hue_range.start, 0, value_range.start);
-    Scalar upper_bound(hue_range.end, 255, value_range.end);
+    Scalar upper_bound(hue_range.end, 256, value_range.end);
     inRange(image_HSV, lower_bound, upper_bound, mask);
 
+    // convert to grayscale and brighten up the selected pixels
     cvtColor( filtered, result, COLOR_BGR2GRAY );
-    add(result, Scalar(40), result, mask);
+    add(result, Scalar(delta_brightness), result, mask);
 }
 
+/**
+ * Print the parameters for debugging purposes.
+ * @param filter_params pointer to DetectionParams struct containing preprocessing and detection parameters
+ */
 void print_parameters(DetectionParams *filter_params)
 {
     cout << "hue range: " << filter_params->hue_range << endl;
@@ -121,5 +152,6 @@ void print_parameters(DetectionParams *filter_params)
     cout << "filter_sigma: " << filter_params->filter_sigma << endl;
     cout << "min_neighbors: " << filter_params->min_neighbors << endl;
     cout << "min_size: " << filter_params->min_size << endl;
+    cout << "delta_brightness: " << filter_params->delta_brightness << endl;
     cout << endl;
 }
